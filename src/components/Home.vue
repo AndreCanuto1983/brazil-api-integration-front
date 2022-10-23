1ad86ad90619a0cce9ca27c2889864fb7b8eac94
André <andrecanuto@outlook.com.br>
2022-10-23 16:51:21 -0300

    feat: ajustando as margens dos cards

--- src/components/Home.vue
+++ src/components/Home.vue
@@ -1,49 +1,35 @@
 <template>
   <b-container>
     <b-row align-h="center">
-
-      <b-card no-body class="overflow-hidden shadow p-3 mb-5 bg-white rounded cardAdjust effectZoom" 
-      style="max-width: 350px" 
-      @click.prevent="goBooks">      
-        <b-row no-gutters>
-          <b-col>
-            <b-card-img
-              src="../static/img/SearchBooks.jpg"
-              alt="Books"
-              class="rounded-0"
-            ></b-card-img>
-          </b-col>
-          <b-col>
-            <b-card-body title="Livros">
-              <b-card-text>
-                Buscar informações de livros através do ISBN.
-              </b-card-text>
-            </b-card-body>
-          </b-col>
-        </b-row>
-      </b-card>&emsp;
-
-      <b-card no-body class="overflow-hidden shadow p-3 mb-5 bg-white rounded cardAdjust effectZoom" 
-      style="max-width: 350px"
-      @click.prevent="goCompany">
-        <b-row no-gutters>
-          <b-col>
-            <b-card-img
-              src="../static/img/SearchCnpj.jpg"
-              alt="Company"
-              class="rounded-0"
-            ></b-card-img>
-          </b-col>
-          <b-col>
-            <b-card-body title="Empresas">
-              <b-card-text>
-                Busca de informações de empresas através do cnpj.
-              </b-card-text>
-            </b-card-body>
-          </b-col>
-        </b-row>
+      <b-card
+        title="Livros"
+        img-src="../static/img/SearchBooks.jpg"
+        img-alt="Books"
+        img-top
+        tag="article"
+        style="max-width: 20rem"
+        class="overflow-hidden shadow p-3 mb-5 bg-white rounded cardAdjust effectZoom"
+        @click.prevent="goBooks"
+      >
+        <b-card-text>
+          Buscar informações de livros através do ISBN.
+        </b-card-text>
       </b-card>
 
+      <b-card
+        title="Empresas"
+        img-src="../static/img/SearchCnpj.jpg"
+        img-alt="Company"
+        img-top
+        tag="article"
+        style="max-width: 20rem"
+        class="overflow-hidden shadow p-3 mb-5 bg-white rounded cardAdjust effectZoom"
+        @click.prevent="goCompany"
+      >
+        <b-card-text>
+          Busca de informações de empresas através do cnpj.
+        </b-card-text>
+      </b-card>
     </b-row>
   </b-container>
 </template>
@@ -52,36 +38,37 @@
 export default {
   components: {},
   methods: {
-    goBooks(){      
-      this.$router.push({ path: "/books" });      
+    goBooks() {
+      this.$router.push({ path: "/books" });
     },
-    goCompany(){      
-      this.$router.push({ path: "/company" });      
-    }
+    goCompany() {
+      this.$router.push({ path: "/company" });
+    },
   },
 };
 </script>
 
 <style scoped>
 .cardAdjust {
-  border-radius: 8px;
-  margin-top: 5%;  
+  border-radius: 8px;  
+  margin: 10px;
+  margin-top: 20px !important;
 }
 
 .cardAdjust:hover {
   cursor: pointer;
 }
 
-.effectZoom{
+.effectZoom {
   -moz-transition: all 0.3s;
-	-webkit-transition: all 0.3s;
-	transition: all 0.3s;
+  -webkit-transition: all 0.3s;
+  transition: all 0.3s;
 }
 
-.effectZoom:hover{  
+.effectZoom:hover {
   -moz-transform: scale(1.1);
-	-webkit-transform: scale(1.1);
-	transform: scale(1.1);
+  -webkit-transform: scale(1.1);
+  transform: scale(1.1);
   z-index: 1;
 }
 </style>
