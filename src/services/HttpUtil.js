d43ec1a8483126f0f66436d148e028fe30f5e2e7
André <andrecanuto@outlook.com.br>
2022-11-11 16:17:26 -0300

    refact: Cors

--- src/services/HttpUtil.js
+++ src/services/HttpUtil.js
@@ -6,6 +6,7 @@ import axios from 'axios';
 const http = axios.create({
   baseURL: window.location.host.includes('localhost') ? 'http://localhost:49168' : 'pospucback.ddns.net:5000',
   headers: {
+    'Access-Control-Allow-Origin': 'pospucback.ddns.net:5000',
     'Accept': '*/*',     
     'Content-Type': 'application/json'
     //'Authorization': authorization.auth() !== null ? `Bearer ${authorization.auth()}` : ''
