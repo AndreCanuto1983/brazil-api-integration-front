6c1708a74d2526ccb84e46859d70f0dc8041fd67
André <andrecanuto@outlook.com.br>
2022-12-26 12:40:27 -0300

    Update api route

--- src/services/HttpUtil.js
+++ src/services/HttpUtil.js
@@ -4,7 +4,7 @@ import axios from 'axios';
 //const authorization = new Authorization();
 
 const http = axios.create({
-  baseURL: window.location.host.includes('localhost') ? 'http://localhost:5000' : 'pospucback.ddns.net:5000',
+    baseURL: window.location.host.includes('localhost') ? 'http://localhost:5000' : 'http://pospuctcc.ddns.net:5000',
   headers: {
     'Accept': '*/*',     
     'Content-Type': 'application/json'    
